50111286583bc425b3dd891a5acf64f0ff6229b7
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-08-15 21:33:14 +0600

    abstraction applied to notification & it's child classes

--- TwitterClone/TwitterClone.Domain/Entities/CommentNotification.cs
+++ TwitterClone/TwitterClone.Domain/Entities/CommentNotification.cs
@@ -19,5 +19,10 @@
             var baseRecord = base.DescribeRecord();
             return $"{baseRecord} - CommentByUserId: {CommentByUserId}";
         }
+
+        public override string GetMessage()
+        {
+            return $"User with ID {CommentByUserId} commented on your post";
+        }
     }
 }
